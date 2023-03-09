5809b6e25394fb08beb6cf585eb0f1f69ad38181
Davit <dav0709@icloud.com>
2023-03-10 01:37:42 +0400

    test

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        Console.Write("Введите число: ");
+        int number = int.Parse(Console.ReadLine());
+
+        if (number % 2 == 0) {
+            Console.WriteLine("Число {0} является чётным.", number);
+        } else {
+            Console.WriteLine("Число {0} не является чётным.", number);
+        }
+    }
+}
+
